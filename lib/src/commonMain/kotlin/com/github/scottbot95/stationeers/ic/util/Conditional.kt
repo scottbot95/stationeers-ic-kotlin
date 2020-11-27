d0b311643c7da8173e41b564543eae305c028b58
Scott Techau <scottbot95@gmail.com>
2020-11-27 11:23:12 -0800

    Flush out available Conditionals.
    Also convert some of the TODOs into issues.

--- lib/src/commonMain/kotlin/com/github/scottbot95/stationeers/ic/util/Conditional.kt
+++ lib/src/commonMain/kotlin/com/github/scottbot95/stationeers/ic/util/Conditional.kt
@@ -34,50 +34,50 @@ class NotEqualToZero(a: ScriptValue<*>) : Conditional("nez", a) {
     override val inverse: Conditional by lazy { EqualToZero(a) }
 }
 
-class Approximately(a: ScriptValue<*>, b: ScriptValue<*>) : Conditional("ap") {
-    override val inverse: Conditional by lazy { NotApproximately(a, b) }
+class ApproximatelyEqual(a: ScriptValue<*>, b: ScriptValue<*>) : Conditional("ap") {
+    override val inverse: Conditional by lazy { NotApproximatelyEqual(a, b) }
 }
 
-class ApproximatelyZero(a: ScriptValue<*>, b: ScriptValue<*>) : Conditional("ap") {
-    override val inverse: Conditional by lazy { NotApproximatelyZero(a, b) }
+class ApproximatelyEqualZero(a: ScriptValue<*>) : Conditional("ap") {
+    override val inverse: Conditional by lazy { NotApproximatelyEqualZero(a) }
 }
 
-class NotApproximately(a: ScriptValue<*>, b: ScriptValue<*>) : Conditional("na") {
-    override val inverse: Conditional by lazy { Approximately(b, a) }
+class NotApproximatelyEqual(a: ScriptValue<*>, b: ScriptValue<*>) : Conditional("na") {
+    override val inverse: Conditional by lazy { ApproximatelyEqual(b, a) }
 }
 
-class NotApproximatelyZero(a: ScriptValue<*>, b: ScriptValue<*>) : Conditional("na") {
-    override val inverse: Conditional by lazy { ApproximatelyZero(b, a) }
+class NotApproximatelyEqualZero(a: ScriptValue<*>) : Conditional("na") {
+    override val inverse: Conditional by lazy { ApproximatelyEqualZero(a) }
 }
 
 class GreaterThan(a: ScriptValue<*>, b: ScriptValue<*>) : Conditional("gt", a, b) {
     override val inverse: Conditional by lazy { LessThanEqualTo(a, b) }
 }
 
-class GreaterThanZero(a: ScriptValue<*>, b: ScriptValue<*>) : Conditional("gt", a, b) {
-    override val inverse: Conditional by lazy { LessThanEqualToZero(a, b) }
+class GreaterThanZero(a: ScriptValue<*>) : Conditional("gt", a) {
+    override val inverse: Conditional by lazy { LessThanEqualToZero(a) }
 }
 
 class GreaterThanEqualTo(a: ScriptValue<*>, b: ScriptValue<*>) : Conditional("ge", a, b) {
     override val inverse: Conditional by lazy { LessThan(a, b) }
 }
 
-class GreaterThanEqualToZero(a: ScriptValue<*>, b: ScriptValue<*>) : Conditional("ge", a, b) {
-    override val inverse: Conditional by lazy { LessThanZero(a, b) }
+class GreaterThanEqualToZero(a: ScriptValue<*>) : Conditional("ge", a) {
+    override val inverse: Conditional by lazy { LessThanZero(a) }
 }
 
 class LessThan(a: ScriptValue<*>, b: ScriptValue<*>) : Conditional("gt", a, b) {
     override val inverse: Conditional by lazy { GreaterThanEqualTo(a, b) }
 }
 
-class LessThanZero(a: ScriptValue<*>, b: ScriptValue<*>) : Conditional("gt", a, b) {
-    override val inverse: Conditional by lazy { GreaterThanEqualToZero(a, b) }
+class LessThanZero(a: ScriptValue<*>) : Conditional("gt", a) {
+    override val inverse: Conditional by lazy { GreaterThanEqualToZero(a) }
 }
 
 class LessThanEqualTo(a: ScriptValue<*>, b: ScriptValue<*>) : Conditional("ge", a, b) {
     override val inverse: Conditional by lazy { GreaterThan(a, b) }
 }
 
-class LessThanEqualToZero(a: ScriptValue<*>, b: ScriptValue<*>) : Conditional("ge", a, b) {
-    override val inverse: Conditional by lazy { GreaterThanZero(a, b) }
+class LessThanEqualToZero(a: ScriptValue<*>) : Conditional("ge", a) {
+    override val inverse: Conditional by lazy { GreaterThanZero(a) }
 }
