8d1c3b7d7f451751ddd4b07e1bd430e02e487c0c
Scott Techau <scott.techau@gmail.com>
2022-03-25 12:19:52 -0700

    Bump gradle version to latest nightly

--- lib/core/src/commonMain/kotlin/com/github/scottbot95/stationeers/ic/CompiledICScriptBuilder.kt
+++ lib/core/src/commonMain/kotlin/com/github/scottbot95/stationeers/ic/CompiledICScriptBuilder.kt
@@ -1,5 +1,7 @@
 package com.github.scottbot95.stationeers.ic
 
+import com.github.scottbot95.stationeers.ic.instructions.Instruction
+
 interface CompiledICScriptBuilder {
     val context: CompileContext
 
@@ -16,7 +18,12 @@ interface CompiledICScriptBuilder {
      * @param string The [String] to add to the script
      * @return <code>this</code>
      */
-    fun appendLine(string: String): CompiledICScriptBuilder
+    fun appendLine(statement: ICScriptStatement): CompiledICScriptBuilder
+
+    /**
+     * Append specified [Instruction] to the compiled script
+     */
+    fun appendInstruction(instruction: Instruction): CompiledICScriptBuilder
 
     /**
      * Final export to [String]
