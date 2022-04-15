0ac45c29a0a40a81ae34476a276b34c6a564707b
Scott Techau <scott.techau@gmail.com>
2022-04-15 15:51:22 -0700

    Remove extra copy when reading a variable.
    This was added original to protected against when the variable ident is impure.
    This did not help since idents provided through delegate are always pure.

--- lib/dsl/src/commonMain/kotlin/com/github/scottbot95/stationeers/ic/dsl/VariableDelegate.kt
+++ lib/dsl/src/commonMain/kotlin/com/github/scottbot95/stationeers/ic/dsl/VariableDelegate.kt
@@ -3,16 +3,13 @@ package com.github.scottbot95.stationeers.ic.dsl
 import com.github.scottbot95.stationeers.ic.highlevel.Expression
 import com.github.scottbot95.stationeers.ic.highlevel.NumberType
 import com.github.scottbot95.stationeers.ic.highlevel.defVar
-import com.github.scottbot95.stationeers.ic.highlevel.temp
 import kotlin.reflect.KProperty
 
 class VariableDelegate(
     private val scope: ICScriptBlockScope,
     private val ident: Expression.Ident,
-    private val varType: NumberType
 ) {
-    operator fun getValue(thisRef: Any?, property: KProperty<*>): Expression =
-        Expression.Copy(ident, Expression.Ident(scope.context.scriptContext.temp(varType)))
+    operator fun getValue(thisRef: Any?, property: KProperty<*>): Expression = ident
 
     operator fun setValue(thisRef: Any?, property: KProperty<*>, value: Expression) {
         scope.context.currentScope?.apply {
@@ -29,6 +26,6 @@ class VariableDelegateProvider(
     operator fun provideDelegate(thisRef: Any?, property: KProperty<*>): VariableDelegate {
         val varName = name ?: property.name
         val ident = scope.context.scriptContext.defVar(varName, varType)
-        return VariableDelegate(scope, Expression.Ident(ident), varType)
+        return VariableDelegate(scope, Expression.Ident(ident))
     }
 }
