d34c237254b8b446d2ee66f51e0e22766a277524
Scott Techau <scott.techau@gmail.com>
2022-04-24 15:49:39 -0700

    Refactor jump threading optimization slightly

--- lib/core/src/commonMain/kotlin/com/github/scottbot95/stationeers/ic/ir/optimization/JumpThreading.kt
+++ lib/core/src/commonMain/kotlin/com/github/scottbot95/stationeers/ic/ir/optimization/JumpThreading.kt
@@ -4,6 +4,8 @@ import com.github.scottbot95.stationeers.ic.ir.IRCompilation
 import com.github.scottbot95.stationeers.ic.ir.IRStatement
 import com.github.scottbot95.stationeers.ic.ir.allEntrypoints
 import com.github.scottbot95.stationeers.ic.ir.cond
+import com.github.scottbot95.stationeers.ic.ir.replace
+import com.github.scottbot95.stationeers.ic.util.toInt
 import mu.KotlinLogging
 import kotlin.reflect.KMutableProperty0
 
@@ -14,9 +16,10 @@ object JumpThreading : IROptimization {
         var changes = 0
 
         compilation.allEntrypoints.forEach { statement ->
-            changes += optimizeRegisterReuse(statement) { this::next }
-            changes += optimizeRegisterReuse(statement) { this::cond }
-            changes += optimizeConstantCondition(statement)
+            changes += statement.optimizeRegisterReuse { this::next }
+            changes += statement.optimizeRegisterReuse { this::cond }
+            changes += statement.optimizeConstantCondition()
+            changes += statement.optimizeCopyReturn()
         }
 
         return changes > 0
@@ -25,22 +28,22 @@ object JumpThreading : IROptimization {
     /**
      * If a test for a register is immediately followed by another test for the same register, skip the second test
      */
-    private fun optimizeRegisterReuse(
-        statement: IRStatement,
+    private fun IRStatement.optimizeRegisterReuse(
         next: IRStatement.() -> KMutableProperty0<IRStatement?>
     ): Int {
+        if (this !is IRStatement.ConditionalStatement) return 0
+
         var changes = 0
-        val nextProp = statement.next()
+        val nextProp = next()
 
         while (
-            (statement is IRStatement.ConditionalStatement) &&
-            (statement::class == nextProp.get()?.let { it::class }) &&
-            (statement.check == (nextProp as? IRStatement.ConditionalStatement)?.check) &&
+            (this::class == nextProp.get()?.let { it::class }) &&
+            (check == (nextProp as? IRStatement.ConditionalStatement)?.check) &&
             (nextProp != nextProp.get()?.next())
         ) {
             // Checks match so outcomes with match
             nextProp.set(nextProp.get()?.next()?.get())
-            logger.debug { "${statement.opCode} -> ${nextProp.get()?.opCode} `${nextProp.name}` case threaded" }
+            logger.debug { "$opCode -> ${nextProp.get()?.opCode} `${nextProp.name}` case threaded" }
             changes++
         }
 
@@ -50,18 +53,16 @@ object JumpThreading : IROptimization {
     /**
      * If a literal load is immediately followed by a conditional jump, hardcode the jump
      */
-    private fun optimizeConstantCondition(statement: IRStatement): Int {
+    private fun IRStatement.optimizeConstantCondition(): Int {
+        if (this !is IRStatement.Init || ident != null) return 0
+
         var changes = 0
 
-        while (
-            (statement is IRStatement.Init) &&
-            (statement.ident == null) &&
-            ((statement.next as? IRStatement.ConditionalStatement)?.check == statement.reg)
-        ) {
-            statement.next = if ((statement.next as IRStatement.ConditionalStatement).matches(statement.value)) {
-                statement.next?.cond
+        while ((next as? IRStatement.ConditionalStatement)?.check == reg) {
+            next = if ((next as IRStatement.ConditionalStatement).matches(value)) {
+                next?.cond
             } else {
-                statement.next?.next
+                next?.next
             }
             logger.debug { "Constant conditional jump elided." }
             changes++
@@ -69,4 +70,17 @@ object JumpThreading : IROptimization {
 
         return changes
     }
+
+    /**
+     * If a copy is followed by a return that consumes the copy result, change the copy into a return.
+     * This leaves an extra return, but that will be cleaned up the by the nop trimming
+     *
+     * TODO this doesn't really fit with jump threading. Find somewhere else to put it?
+     */
+    private fun IRStatement.optimizeCopyReturn(): Int =
+        if (this is IRStatement.Copy && (next as? IRStatement.Return)?.result == dest) {
+            replace(IRStatement.Return(src)).toInt()
+        } else {
+            0
+        }
 }
